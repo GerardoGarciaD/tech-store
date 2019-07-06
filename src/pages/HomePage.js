0df82993d4936f832d0e92b0f0caf20600f4d3b0
Gerardo Garcia <geraz26@hotmail.com>
2019-07-06 00:08:47 -0500

    Se manda a llamar el componente Hero y se mandan Props

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
     <>
-      <h1>Hello from Home Page</h1>
+      {/* Se mandan las props al componente Hero */}
+      <Hero title="Awesome gadgets" max="true">
+        <Link to="/products">Our products</Link>
+      </Hero>
     </>
   );
 }
