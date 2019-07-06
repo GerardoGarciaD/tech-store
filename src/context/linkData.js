157f19ff2bc379d369c017173c0c10c7f77a720c
Gerardo Garcia <geraz26@hotmail.com>
2019-07-05 21:04:45 -0500

    Creacion de array con para las direcciones de los li

---
+++ src/context/linkData.js
@@ -0,0 +1,28 @@
+// Links para el sidebar
+export const linkData = [
+  {
+    id: 1,
+    text: "Home",
+    path: "/"
+  },
+  {
+    id: 2,
+    text: "About",
+    path: "/about"
+  },
+  {
+    id: 3,
+    text: "Products",
+    path: "/products"
+  },
+  {
+    id: 4,
+    text: "Contact",
+    path: "/contact"
+  },
+  {
+    id: 5,
+    text: "Cart",
+    path: "/cart"
+  }
+];
