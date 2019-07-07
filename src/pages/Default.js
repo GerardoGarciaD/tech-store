b93e043d0a5b3e5efdb6f9b49ff9241afcccf14e
Gerardo Garcia <geraz26@hotmail.com>
2019-07-06 19:11:04 -0500

    Creacion de pagina default o pagina de error

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,9 +1,23 @@
+// Componente que aparece cuando se quiere buscar una pagina que no existe
 import React from "react";
+// Componente para añadir la imagen grande
+import Hero from "../components/Hero";
+// Imagen por defecto cuando no se encuentra la pagina
+import defaultBcg from "../images/defaultBcg.jpeg";
+// Componente para redireccionar a las paginas
+import { Link } from "react-router-dom";
 
 export default function Default() {
   return (
     <>
-      <h1>Hello from Default Page</h1>
+      {/* Se renderiza el componente Hero, en donde se mandalas props img, title y max, */}
+      <Hero img={defaultBcg} title="404" max="true">
+        <h2 className="text-uppercase"> page not found</h2>
+
+        <Link to="/" className="main-link" style={{ margin: "2rem" }}>
+          Return to home
+        </Link>
+      </Hero>
     </>
   );
 }
