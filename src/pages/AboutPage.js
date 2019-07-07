0ea830139d2b9ea6bfa09d63c893b74b6efb1a70
Gerardo Garcia <geraz26@hotmail.com>
2019-07-06 20:23:02 -0500

    Se renderizan los componentes Hero e Info

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,16 @@
 import React from "react";
-
+// Se importa el componente que muestra la informacion de About
+import Info from "../components/AboutPage/Info";
+// Se importa el componente Hero, el que se encarga de mostrar las imagenes en tamaño grande
+import Hero from "../components/Hero";
+import aboutBcg from "../images/aboutBcg.jpeg";
+import Title from "../components/Title";
 export default function AboutPage() {
   return (
     <>
-      <h1>Hello from About Page</h1>
+      <Hero img={aboutBcg} />
+      {/* Se manda a llamar el componente info, que es que contiene toda la informacion de AboutUS */}
+      <Info />
     </>
   );
 }
