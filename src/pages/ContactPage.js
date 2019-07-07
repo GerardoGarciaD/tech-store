7d756ac634b2d301ce87657120061a4c3cad0a08
Gerardo Garcia <geraz26@hotmail.com>
2019-07-06 21:21:57 -0500

    Se mandan a llamar los componentes Hero y contact

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,18 @@
 import React from "react";
+// Componente para imagen grande
+import Hero from "../components/Hero";
+// Import de la imagen a mostrar
+import contactImg from "../images/contactBcg.jpeg";
+// Se importa el componente que contiene toda la informacion para el componente ContactPage
+import Contact from "../components/ContactPage/Contact";
 
 export default function ContactPage() {
   return (
     <>
-      <h1>Hello from Contact Page</h1>
+      {/* Se manda a llamar el componente Hero en donde se manda como prop la imagen a ser mostrada */}
+      <Hero img={contactImg} />
+      {/* Se manda a llamar el componente que contiene la informacion del componente contacto */}
+      <Contact> </Contact>
     </>
   );
 }
