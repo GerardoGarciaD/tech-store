3ddf9fc3aaf9ca5d04a31843fce8b205ce8e5b1d
Gerardo Garcia <geraz26@hotmail.com>
2019-07-06 22:13:59 -0500

    Array para los iconos del footer

---
+++ src/context/socialData.js
@@ -0,0 +1,24 @@
+// Componente para los iconos del footer
+import React from "react";
+
+import { FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
+
+// Se crea el array que contendra los iconos para el footer
+
+export const socialData = [
+  {
+    id: 1,
+    icon: <FaFacebook className="icon" />,
+    url: "https://www.facebook.com"
+  },
+  {
+    id: 2,
+    icon: <FaTwitter className="icon" />,
+    url: "https://www.twitter.com"
+  },
+  {
+    id: 3,
+    icon: <FaLinkedin className="icon" />,
+    url: "https://www.linkedin.com"
+  }
+];
