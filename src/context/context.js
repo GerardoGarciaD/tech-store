75d42d3cdab3c6e5f89a742ba378d0454ddb642f
Gerardo Garcia <geraz26@hotmail.com>
2019-07-09 21:27:26 -0500

    Metodos para hacer destruguring de la informacion de los productos

--- src/context/context.js
+++ src/context/context.js
@@ -6,6 +6,9 @@ import { linkData } from "./linkData";
 // Se hace el import de los iconos para las redes sociales
 import { socialData } from "./socialData";
 
+// Se importa la informacion de los items
+import { items } from "./productData";
+
 const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
@@ -13,12 +16,93 @@ class ProductProvider extends Component {
   state = {
     sidebarOpen: false,
     cartOpen: false,
-    cartItems: 0,
     // se guarda el array que para los links
     links: linkData,
     // se guarda el array para los iconos de las redes sociales
     socialIcons: socialData,
-    cart: []
+    cart: [],
+    // Variables para poder controlar el cart, los productos, etc.
+    cartItems: 0,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0,
+    storeProducts: [],
+    filteredProducts: [],
+    featuredProducts: [],
+    singleProduct: {},
+    loading: true
+  };
+
+  // Se crea un metodo, (componentDidMount), que este tipo de componentes se utilizan una vez que se haya terminado
+  // de "montar" el componente
+
+  componentDidMount() {
+    // Se manda a llamar el metodo en donde se manda como parametro todos los datos de los productos (items)
+    this.setProducts(items);
+  }
+
+  // Se crea un metodo para obtener los campos del array que contiene toda la informacion de los productos
+  // esto para crear un producto con una estructura mas "amigable"
+  setProducts = products => {
+    // Se recorre todo el array que se recibe como parametro
+    let storeProducts = products.map(item => {
+      // se hace destructuring para obtener los valores que estan dentro del array
+      const { id } = item.sys;
+
+      // Se hace destructuring para obtener el url de la imagen mas facilmente al momento de crear un nuevo producto
+      const image = item.fields.image.fields.file.url;
+
+      /* Se crea un nuevo producto que contendrá el id que se obtuvo anteriormente, y se utiliza spread operetaros(...)
+      para guardar toda la informacion del objeto item.fields (title,price,company,etc) */
+      const product = { id, ...item.fields, image };
+      return product;
+    });
+    // Verificacion que se crean y guardan los productos correctamente
+    // console.log(storedProducts);
+
+    // Se encuentran los productos "destacados", se recorre el stored product para encontrar los que tengan el campo featured = true
+    let featuredProducts = storeProducts.filter(item => item.featured === true);
+
+    // se cambian los valores  que se encuentran en el estado
+
+    this.setState({
+      storeProducts,
+      filteredProducts: storeProducts,
+      featuredProducts,
+      cart: this.getStorageCart(),
+      singleProduct: this.getStorageProduct(),
+      loadig: false
+    });
+  };
+
+  // obtener la informacion del carrito de manera local
+  getStorageCart = () => {
+    return [];
+  };
+
+  // obtener la informacion de un product de manera local
+  getStorageProduct = () => {
+    return {};
+  };
+
+  // Obtener el total de manera local
+
+  getTotals = () => {};
+
+  // add Totals
+  getTotals = () => {};
+
+  // sincronizacion de los items
+  syncTotals = () => {};
+
+  // añadir al carrito
+  addToCart = id => {
+    console.log(`add to cart ${id}`);
+  };
+
+  // set single product
+  setSingleProduct = id => {
+    console.log(`single product ${id}`);
   };
 
   //   Funciones que se van a utilizar durante el proyecto
@@ -42,13 +126,15 @@ class ProductProvider extends Component {
   render() {
     return (
       <ProductContext.Provider
-        //   estos son los valores que estaran disponibles en todo el proyecto
+        //   estos son los valores y metodos que estaran disponibles en todo el proyecto
         value={{
           ...this.state,
           handleSidebar: this.handleSidebar,
           handleCart: this.handleCart,
           closeCart: this.closeCart,
-          openCart: this.openCart
+          openCart: this.openCart,
+          addToCart: this.addToCart,
+          setSingleProduct: this.setSingleProduct
         }}
       >
         {/* Se pone esta linea de codigo para indicar que el contexto va a abarcar los hijos que esten dentro del context */}
