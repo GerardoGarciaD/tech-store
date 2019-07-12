40f110a2962822a4ea91c58daf66c70c7fd374a4
Gerardo Garcia <geraz26@hotmail.com>
2019-07-11 19:03:29 -0500

    Añadiendo contenido del componente

--- src/components/SideCart.js
+++ src/components/SideCart.js
@@ -2,19 +2,44 @@ import React from "react";
 
 // Se hace import del contexto creado
 import { ProductConsumer } from "../context";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export default function SideCart() {
   return (
     <ProductConsumer>
       {value => {
-        const { cartOpen, closeCart, cart } = value;
+        const { cartOpen, closeCart, cart, cartTotal } = value;
 
         return (
           // Se manda a llamar al componente CartWrapper y se manda una prop, show, que es para hacer los
           // estilos condicionales, (hacer que aparezca o desaparezca el siedcart)
           <CartWrapper show={cartOpen} onClick={closeCart}>
-            <p>Cart items</p>
+            <ul>
+              {cart.map(item => {
+                return (
+                  <li key={item.id} className="cart-item">
+                    {/* <img src={item.image} width="35" alt="Cart" /> */}
+                    <img src={`../${item.image}`} width="35" alt="Cart" />
+
+                    <div className="mt-3">
+                      <h6 className="text-uppercase">{item.title}</h6>
+                      <h6 className="text-title text-capitalize">
+                        Amount: {item.count}
+                      </h6>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+            <h4 className="text-capitalize text-main">
+              cart total: ${cartTotal}
+            </h4>
+            <div className="text-center my-5">
+              <Link to="/cart" className="main-link">
+                Cart page
+              </Link>
+            </div>
           </CartWrapper>
         );
       }}
@@ -39,5 +64,16 @@ const CartWrapper = styled.div`
 
   @media (min-width: 576px) {
     width: 20rem;
+  }
+
+  overflow: scroll;
+  padding: 2rem;
+
+  ul {
+    padding: 0 !important;
+  }
+
+  .cart-item {
+    list-style-type: none;
   }
 `;
