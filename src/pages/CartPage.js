0c179cd7a562ad8faf1612b1e6937f6f86f62cc1
Gerardo Garcia <geraz26@hotmail.com>
2019-07-11 23:07:36 -0500

    Se añade el componente que contiene todos los otros componentes del carrito

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,9 +1,14 @@
 import React from "react";
+// Se  importa el componene que contiene todos los componentes para esta pagina
+import CartSection from "../components/CartPage";
+import Hero from "../components/Hero";
+import cartBcg from "../images/storeBcg.jpeg";
 
 export default function CartPage() {
   return (
     <>
-      <h1>Hello from Cart Page</h1>
+      <Hero img={cartBcg} />
+      <CartSection />
     </>
   );
 }
